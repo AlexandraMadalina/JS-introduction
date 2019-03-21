1b4282671e54e446b65c194b379b96e9a19196bf
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-03-21 23:01:37 +0100

    When you click the button again, the picture should disappear

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   document.getElementById("doge").innerHTML = "Woof!";
+
 }
 
 function hover() {
@@ -7,20 +8,13 @@ function hover() {
   document.getElementById("iconDoge").style.display = "inline";
 }
 
-function showPicture() {
-  var dogImg = document.createElement("IMG");
-  var center = document.getElementById("center");
-  center.style.position="relative";
-  dogImg.setAttribute("src", "doge.png");
-  dogImg.setAttribute("width", "auto");
-  dogImg.setAttribute("height", "auto");
-  dogImg.setAttribute("alt", "doge_picture");
-  dogImg.setAttribute("position","absolute");
-  dogImg.setAttribute("top","0px");
-  dogImg.setAttribute("left","0px");
-  dogImg.style.zIndex = "1";
-  center.appendChild(dogImg);
-  var iconDoge=document.getElementById("iconDoge");
-    iconDoge.style.zIndex = "2";
-    //cant make the gog stay in the back and the icon in the middle
-}
+
+
+  function showPicture() {
+var dogImg = document.getElementById("dogImg");
+   if (dogImg.style.display == 'none') {
+      dogImg.style.display = 'block';
+    } else {
+      dogImg.style.display = 'none';
+    }
+  }
